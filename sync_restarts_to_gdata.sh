31cbaa43ffcc38d8281f026b440befbc18e98a43
Hannah Dawson <62230733+hrsdawson@users.noreply.github.com>
2023-06-14 16:13:10 +1000

    update PBS

---
+++ sync_restarts_to_gdata.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -q copyq
+#PBS -l ncpus=1
+#PBS -l wd
+#PBS -l storage=gdata/ik11+scratch/v45+scratch/e14
+#PBS -l walltime=2:00:00,mem=2GB
+#PBS -P e14
+#PBS -N restarts_to_gdata
+
+source sync_output_to_gdata.sh # to define GDATADIR and cd archive
+
+rsync -vrltoD --safe-links restart* ${GDATADIR}
