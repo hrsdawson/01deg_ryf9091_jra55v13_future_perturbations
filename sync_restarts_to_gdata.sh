61a8218b72cfb7ed413b4887aa7f4145b8fbfffd
Hannah Dawson <62230733+hrsdawson@users.noreply.github.com>
2023-12-01 15:28:16 +1100

    update storage flags for syncing

--- sync_restarts_to_gdata.sh
+++ sync_restarts_to_gdata.sh
@@ -2,7 +2,7 @@
 #PBS -q copyq
 #PBS -l ncpus=1
 #PBS -l wd
-#PBS -l storage=gdata/ik11+scratch/v45+scratch/e14
+#PBS -l storage=gdata/ik11+gdata/e14+gdata/cj50+scratch/v45+scratch/e14+scratch/oz91
 #PBS -l walltime=2:00:00,mem=2GB
 #PBS -P e14
 #PBS -N restarts_to_gdata
