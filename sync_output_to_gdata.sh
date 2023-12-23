4a2108b9f0e3a8edc8f2ef72138934c598e24163
Hannah Dawson <62230733+hrsdawson@users.noreply.github.com>
2023-12-23 22:01:17 +1100

    update GDATADIR and walltime for syncing

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -3,14 +3,14 @@
 #PBS -l ncpus=1
 #PBS -l wd
 #PBS -l storage=gdata/ik11+scratch/e14+gdata/e14
-#PBS -l walltime=12:00:00,mem=2GB
+#PBS -l walltime=24:00:00,mem=2GB
 #PBS -P e14
 #PBS -N output_to_gdata
 
 # Set this directory to something in /g/data3/hh5/tmp/cosima/
 # Make a unique path for your set of runs.
 # DOUBLE-CHECK IT IS UNIQUE SO YOU DON'T OVERWRITE EXISTING OUTPUT!
-GDATADIR=/g/data/e14/hd4873/access-om2-01/01deg_jra55v13_ssp585_windthermalmw
+GDATADIR=/g/data/e14/hd4873/access-om2-01/01deg_jra55v13_ssp585_mw
 
 mkdir -p ${GDATADIR}
 cd archive
